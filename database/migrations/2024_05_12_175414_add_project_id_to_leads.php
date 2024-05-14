b63bc9d65ad3cc56f5d0cffa105463b3280cd80f
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-05-14 09:08:03 -0700

    Create 2024_05_12_175414_add_project_id_to_leads.php

---
+++ database/migrations/2024_05_12_175414_add_project_id_to_leads.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id')->nullable();
+
+            // Define the foreign key constraint
+            $table->foreign('project_id')
+                  ->references('id')
+                  ->on('projects')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->dropForeign(['project_id']);
+
+            // Drop the project_id column
+            $table->dropColumn('project_id');
+        });
+    }
+};
