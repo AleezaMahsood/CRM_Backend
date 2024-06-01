985339d4e8588a45e34bfa35e302036eac2a697c
AleezaMahsood <aleeza.mahsood18@gmail.com>
2024-06-01 23:14:41 +0500

    Changes Committed

--- app/Http/Controllers/Api/CampaignController.php
+++ app/Http/Controllers/Api/CampaignController.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Request;
 use App\Models\Campaigns;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use App\Mail\TestEmail;
+use App\Models\leads;
+use Illuminate\Support\Facades\Mail; 
 
 class CampaignController extends Controller
 {
@@ -24,12 +27,12 @@ class CampaignController extends Controller
     public function store(Request $request)
     {
         $validated = Validator::make($request->all(),[
-            'campaign_name' => 'required|string|max:150|unique:campaigns,campaign_name',
+            'campaign_name' => 'required|string|max:150',
             'description' => 'required|string|max:255',
             'start_date' => 'nullable|date',
             'end_date' => 'nullable|date|after:start_date',
-            'expected_revenue' => 'required|integer',
-            'actual_cost' => 'required|integer' ]);
+            'expected_revenue' => 'required|string|max:255',
+            'actual_cost' => 'required|string|max:255' ]);
           if($validated->fails()){
               return response()->json([
                   'status'=>422,
@@ -44,8 +47,23 @@ class CampaignController extends Controller
                   'actual_cost' => $request->actual_cost,
                   'expected_revenue' => $request->expected_revenue
               ]); 
-              
+                        
           }
+   
+    //$leads = leads::all();
+
+   // foreach ($leads as $lead) {
+    //    Mail::to($lead->email)->send(new TestEmail($campaigns));
+   // }
+   $leadEmails = leads::pluck('email');
+
+// Filter out valid email addresses (e.g., remove dummy or invalid emails)
+$validEmails = $leadEmails->filter(function ($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+});
+
+// Send the email to valid recipients
+Mail::to($validEmails)->send(new TestEmail($campaigns));
           if($campaigns){
               //$article->category()->attach($request->categories);
               return response()->json([
